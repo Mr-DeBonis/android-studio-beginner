8313c52da1563c2ce41fe5abc775f6291e594ed7
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-07-05 13:28:17 -0400

    Add locale tutorial

---
+++ XMLFilesSection1/app/src/main/java/org/mrdebonis/xmlfilessection1/MainActivity.java
@@ -0,0 +1,20 @@
+package org.mrdebonis.xmlfilessection1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtHello;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText(getString(R.string.hello));
+    }
+}
