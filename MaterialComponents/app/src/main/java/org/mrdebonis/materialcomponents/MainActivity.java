6b3bc463902973f54a2ef4579d70e9df55d7fb0c
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-07-07 15:40:49 -0400

    Add cardView tutorial

--- MaterialComponents/app/src/main/java/org/mrdebonis/materialcomponents/MainActivity.java
+++ MaterialComponents/app/src/main/java/org/mrdebonis/materialcomponents/MainActivity.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private ConstraintLayout parent;
     private Button btnShowSnackbar;
+    private MaterialCardView cardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
         parent = findViewById(R.id.parentLayout);
         btnShowSnackbar = findViewById(R.id.button);
+
+        cardView = findViewById(R.id.cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Card Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btnShowSnackbar.setOnClickListener(new View.OnClickListener() {
             @Override
