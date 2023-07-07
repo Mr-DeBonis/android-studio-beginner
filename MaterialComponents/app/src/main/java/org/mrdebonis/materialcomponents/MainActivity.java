869b6af8e351babe8959c5d4c58c0f547e89b7c6
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-07-07 15:28:05 -0400

    Add colored snackbar

---
+++ MaterialComponents/app/src/main/java/org/mrdebonis/materialcomponents/MainActivity.java
@@ -0,0 +1,48 @@
+package org.mrdebonis.materialcomponents;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout parent;
+    private Button btnShowSnackbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        parent = findViewById(R.id.parentLayout);
+        btnShowSnackbar = findViewById(R.id.button);
+
+        btnShowSnackbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSnackbar();
+            }
+        });
+
+    }
+
+
+    public void showSnackbar() {
+        Snackbar.make(parent, "This is a Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Retry Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setActionTextColor(getResources().getColor(R.color.ColorAccent))
+                .setTextColor(Color.YELLOW)
+                .show();
+    }
+}
