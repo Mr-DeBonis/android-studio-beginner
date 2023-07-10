1ff2a55bfe94684202b9dc41a427e9e394be1624
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-07-10 09:53:23 -0400

    Add RecyclerView Part 2 - Add data to recyclerView

--- RecyclerView/app/src/main/java/org/mrdebonis/recyclerview/ContactsRecViewAdapter.java
+++ RecyclerView/app/src/main/java/org/mrdebonis/recyclerview/ContactsRecViewAdapter.java
@@ -1,8 +1,12 @@
 package org.mrdebonis.recyclerview;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,20 +16,37 @@ import java.util.ArrayList;
 public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecViewAdapter.ViewHolder> {
 
     private ArrayList<Contact> contacts = new ArrayList<>();
+    private Context context;
 
-    public ContactsRecViewAdapter() {
+    public ContactsRecViewAdapter(Context context) {
+        this.context = context;
 
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.contacts_list_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
+    /**
+     * Changes the properties of UI elements
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.txtName.setText(contacts.get(position).getName());
+        holder.parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, contacts.get(position).getName() + " Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -40,10 +61,12 @@ public class ContactsRecViewAdapter extends RecyclerView.Adapter<ContactsRecView
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView txtName;
+        private RelativeLayout parent;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtName = itemView.findViewById(R.id.txtName);
+            parent = itemView.findViewById(R.id.parent);
         }
     }
 }
--- RecyclerView/app/src/main/java/org/mrdebonis/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/org/mrdebonis/recyclerview/MainActivity.java
@@ -1,9 +1,13 @@
 package org.mrdebonis.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         contactsRecView = findViewById(R.id.contactsRecView);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        contacts.add(new Contact("Margot Robbie", "Margot@gmail.com", "https://gal.img.pmdstatic.net/scale/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FGAL.2F2019.2F09.2F23.2F93587958-1a9b-482c-960e-f0b1cf068faa.2Ejpeg/autox450/quality/65/margot-robbie-lors-de-la-88eme-ceremonie-des-oscars-a-hollywood-le-28-fevrier-2016.jpg"));
+        contacts.add(new Contact("Cillian Murphy", "Cillian@gmail.com", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Cillian_Murphy_Press_Conference_The_Party_Berlinale_2017_02cr.jpg/1200px-Cillian_Murphy_Press_Conference_The_Party_Berlinale_2017_02cr.jpg"));
+        contacts.add(new Contact("Saoirse Roman", "Saoirse@gmail.com", "https://m.media-amazon.com/images/M/MV5BMjExNTM5NDE4NV5BMl5BanBnXkFtZTcwNzczMzEzOQ@@._V1_.jpg"));
+        contacts.add(new Contact("Emma Watson", "Emma@gmail.com", "https://upload.wikimedia.org/wikipedia/commons/7/7f/Emma_Watson_2013.jpg"));
+        contacts.add(new Contact("Christian Bale", "Christian@gmail.com", "https://upload.wikimedia.org/wikipedia/commons/0/0a/Christian_Bale-7837.jpg"));
+
+        ContactsRecViewAdapter adapter = new ContactsRecViewAdapter(this);
+        adapter.setContacts(contacts);
+
+        contactsRecView.setAdapter(adapter);
+        /*
+        There are several LAyout managers:
+        LinearLayoutManager: Specify oreintation: vertical or horizontal
+        GridLAyoutMAnager : Specify columns
+         */
+        contactsRecView.setLayoutManager(new GridLayoutManager(this, 2));
+
     }
 }
