f3a71b386a816be31f4b9ba8149b2a408d620732
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-07-10 11:44:48 -0400

    Add Font tutorial

---
+++ Fonts/app/src/main/java/org/mrdebonis/fonts/MainActivity.java
@@ -0,0 +1,33 @@
+package org.mrdebonis.fonts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtHello;
+    private Button btnChangeFont;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtHello = findViewById(R.id.txtHello);
+        btnChangeFont = findViewById(R.id.btnChangeFont);
+
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.amsterdam);
+
+        btnChangeFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtHello.setTypeface(typeface);
+            }
+        });
+    }
+}
